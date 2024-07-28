12031baa8495bc734851cb84461b97c1a0c60b69
CMacino <123585411+Cas-Student@users.noreply.github.com>
2024-07-28 22:33:29 +0000

    u

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
-const tab = window.open("about:blank", "_blank", true);
-const doc = tab.document;
-
-const ifra = doc.createElement("iframe");
-ifra.src = document.getElementById("url").innerText;
-ifra.style.border = "none";
-ifra.width = "100%";
-ifra.height = "100%";
-
-doc.body.appendChild(ifra);
+function GoTo() {
+    const tab = window.open("about:blank", "_blank", true);
+    const doc = tab.document;
+    const ifra = doc.createElement("iframe");
+    ifra.src = document.getElementById("url").innerText;
+    alert("Going to " + document.getElementById("url").innerText);
+    ifra.style.border = "none";
+    ifra.width = "100%";
+    ifra.height = "100%";
+    doc.body.appendChild(ifra);
+}
