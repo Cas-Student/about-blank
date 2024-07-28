20f727267140320f8184eaa65e2c5af657a9924e
CMacino <123585411+Cas-Student@users.noreply.github.com>
2024-07-28 22:20:02 +0000

    s

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ const tab = window.open("about:blank", "_blank", true);
 const doc = tab.document;
 
 const ifra = doc.createElement("iframe");
-ifra.src = "https://start.hcps.org";
+ifra.src = document.getElementById("url").innerText;
+ifra.style.border = "none";
+ifra.width = "100%";
+ifra.height = "100%";
 
 doc.body.appendChild(ifra);
