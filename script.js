5a902bddd1b054eb76fee5176c87e1b8c3c81f61
CMacino <123585411+Cas-Student@users.noreply.github.com>
2024-07-28 22:19:44 +0000

    s

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ const tab = window.open("about:blank", "_blank", true);
 const doc = tab.document;
 
 const ifra = doc.createElement("iframe");
-ifra.src = "start.hcps.org";
+ifra.src = "https://start.hcps.org";
 
 doc.body.appendChild(ifra);
